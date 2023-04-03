d3b4f1bd9643e1137cf9a1e570ecc1c3651604f8
Igor3550 <igormeloigormelo@gmail.com>
2023-04-03 18:34:52 -0300

    Setting the croosorigin to fix the cors error

--- src/main/java/com/tweteroo/api/controllers/AuthController.java
+++ src/main/java/com/tweteroo/api/controllers/AuthController.java
@@ -3,6 +3,7 @@ package com.tweteroo.api.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import com.tweteroo.api.services.AuthService;
 import jakarta.validation.Valid;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/api/auth/sign-up")
 public class AuthController {
   
